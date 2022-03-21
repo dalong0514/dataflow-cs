32af8946ff2b5286e4731a7d4a7b3dfb653b71cb
dalong <dalong0514@me.com>
2022-03-21 16:40:25 +0800

    增加 Active 静态类

--- dataflow-cs/Commands.cs
+++ dataflow-cs/Commands.cs
@@ -53,5 +53,51 @@ namespace dataflow_cs
                 tr.Commit();
             }
         }
+
+        /// runtime environment.
+        /// </summary>
+        public static class Active
+        {
+            /// <summary>
+            /// Returns the active Editor object.
+            /// </summary>
+            public static Editor Editor
+            {
+                get { return Document.Editor; }
+            }
+            /// <summary>
+            /// Returns the active Document object.
+            /// </summary>
+            public static Document Document
+            {
+                get { return Application.DocumentManager.MdiActiveDocument; }
+            }
+            /// <summary>
+            /// Returns the active Database object.
+            /// </summary>
+            public static Database Database
+            {
+                get { return Document.Database; }
+            }
+            /// <summary>
+            /// Sends a string to the command line in the active Editor
+            /// </summary>
+            /// <param name="message">The message to send.</param>
+            public static void WriteMessage(string message)
+            {
+                Editor.WriteMessage(message);
+            }
+            /// <summary>
+            /// Sends a string to the command line in the active Editor using String.Format.
+            /// </summary>
+            /// <param name="message">The message containing format specifications.</param>
+            /// <param name="parameter">The variables to substitute into the format string.</param>
+            public static void WriteMessage(string message, params object[] parameter)
+            {
+                Editor.WriteMessage(message, parameter);
+            }
+        }
+
+
     }
 }
