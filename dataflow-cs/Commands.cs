32a8b926d67429a1664f887d068b5ac3c6874d1f
dalong <dalong0514@me.com>
2022-03-21 13:52:00 +0800

    init project

---
+++ dataflow-cs/Commands.cs
@@ -0,0 +1,57 @@
+﻿// using Autodesk.AutoCAD.ApplicationServices.Core;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dataflow_cs
+{
+    public class Commands
+    {
+        [CommandMethod("Hello")] 
+        public void Hello()
+        {
+            Editor editor = Application.DocumentManager.MdiActiveDocument.Editor;
+            editor.WriteMessage("Hello, dalong");
+        }
+
+        [CommandMethod("ENTCOLOR1")]
+        public void ChangeEntityColor1()
+        {
+            // Get the various active objects
+            Document document = Application.DocumentManager.MdiActiveDocument;
+            Database database = document.Database;
+            // Create a new transaction
+            Autodesk.AutoCAD.DatabaseServices.Transaction tr = database.TransactionManager.StartTransaction();
+            using (tr)
+            {
+                // Get the block table for the current database
+                var blockTable =
+                (BlockTable)tr.GetObject(database.BlockTableId, OpenMode.ForRead);
+                // Get the model space block table record
+                var modelSpace = (BlockTableRecord)tr.GetObject(blockTable[BlockTableRecord.ModelSpace], OpenMode.ForRead);
+                RXClass circleClass = RXObject.GetClass(typeof(Circle));
+                // Loop through the entities in model space
+                foreach (ObjectId objectId in modelSpace)
+                {
+                    // Look for circles
+                    if (objectId.ObjectClass.IsDerivedFrom(circleClass))
+                    {
+                        var circle = (Circle)tr.GetObject(objectId, OpenMode.ForRead);
+                        if (circle.Radius < 1.0)
+                        {
+                            circle.UpgradeOpen();
+                            circle.ColorIndex = 1;
+                        }
+                    }
+                }
+                tr.Commit();
+            }
+        }
+    }
+}
