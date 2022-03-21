5560da7fbfc3d913a84f0819923256f9c1070446
dalong <dalong0514@me.com>
2022-03-21 19:36:46 +0800

    删除引入CADActive静态类后多余的代码

--- dataflow-cs/Commands.cs
+++ dataflow-cs/Commands.cs
@@ -17,45 +17,12 @@ namespace dataflow_cs
         [CommandMethod("Hello")] 
         public void Hello()
         {
-            Editor editor = Application.DocumentManager.MdiActiveDocument.Editor;
+            Editor editor = CADActive.Editor;
             editor.WriteMessage("Hello, dalong");
         }
 
-        [CommandMethod("ENTCOLOR1")]
-        public void ChangeEntityColor1()
-        {
-            // Get the various active objects
-            Document document = Application.DocumentManager.MdiActiveDocument;
-            Database database = document.Database;
-            // Create a new transaction
-            Transaction tr = database.TransactionManager.StartTransaction();
-            using (tr)
-            {
-                // Get the block table for the current database
-                var blockTable = (BlockTable)tr.GetObject(database.BlockTableId, OpenMode.ForRead);
-                // Get the model space block table record
-                var modelSpace = (BlockTableRecord)tr.GetObject(blockTable[BlockTableRecord.ModelSpace], OpenMode.ForRead);
-                RXClass circleClass = RXObject.GetClass(typeof(Circle));
-                // Loop through the entities in model space
-                foreach (ObjectId objectId in modelSpace)
-                {
-                    // Look for circles
-                    if (objectId.ObjectClass.IsDerivedFrom(circleClass))
-                    {
-                        var circle = (Circle)tr.GetObject(objectId, OpenMode.ForRead);
-                        if (circle.Radius < 1.0)
-                        {
-                            circle.UpgradeOpen();
-                            circle.ColorIndex = 1;
-                        }
-                    }
-                }
-                tr.Commit();
-            }
-        }
-
-        [CommandMethod("ENTCOLOR2")]
-        public void ChangeEntityColor2()
+        [CommandMethod("ENTCOLOR")]
+        public void ChangeEntityColor()
         {
 
             using (var tr = CADActive.Database.TransactionManager.StartTransaction())
