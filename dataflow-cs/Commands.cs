2fd0e70d0b8c80607487c41da6b795bc68d38c5b
dalong <dalong0514@me.com>
2022-03-21 16:59:57 +0800

    静态类重构AutoCAD-API

--- dataflow-cs/Commands.cs
+++ dataflow-cs/Commands.cs
@@ -31,8 +31,7 @@ namespace dataflow_cs
             using (tr)
             {
                 // Get the block table for the current database
-                var blockTable =
-                (BlockTable)tr.GetObject(database.BlockTableId, OpenMode.ForRead);
+                var blockTable = (BlockTable)tr.GetObject(database.BlockTableId, OpenMode.ForRead);
                 // Get the model space block table record
                 var modelSpace = (BlockTableRecord)tr.GetObject(blockTable[BlockTableRecord.ModelSpace], OpenMode.ForRead);
                 RXClass circleClass = RXObject.GetClass(typeof(Circle));
@@ -98,6 +97,35 @@ namespace dataflow_cs
             }
         }
 
+        [CommandMethod("ENTCOLOR2")]
+        public void ChangeEntityColor2()
+        {
+
+            using (var tr = Active.Database.TransactionManager.StartTransaction())
+            {
+
+                // Get the block table for the current database
+                var blockTable = (BlockTable)tr.GetObject(Active.Database.BlockTableId, OpenMode.ForRead);
+                // Get the model space block table record
+                var modelSpace = (BlockTableRecord)tr.GetObject(blockTable[BlockTableRecord.ModelSpace], OpenMode.ForRead);
+                RXClass circleClass = RXObject.GetClass(typeof(Circle));
+                // Loop through the entities in model space
+                foreach (ObjectId objectId in modelSpace)
+                {
+                    // Look for circles
+                    if (objectId.ObjectClass.IsDerivedFrom(circleClass))
+                    {
+                        var circle = (Circle)tr.GetObject(objectId, OpenMode.ForRead);
+                        if (circle.Radius < 1.0)
+                        {
+                            circle.UpgradeOpen();
+                            circle.ColorIndex = 1;
+                        }
+                    }
+                }
+                tr.Commit();
+            }
+        }
 
     }
 }
