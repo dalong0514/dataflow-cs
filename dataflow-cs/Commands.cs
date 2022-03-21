a6e31ab7e543ac4dc78fabc215db2476438c1747
dalong <dalong0514@me.com>
2022-03-21 20:11:08 +0800

    练习委托delegate

--- dataflow-cs/Commands.cs
+++ dataflow-cs/Commands.cs
@@ -51,5 +51,48 @@ namespace dataflow_cs
             }
         }
 
+        public delegate void TransactionFunc(Action<Transaction> tr);
+
+        public void UsingTransaction(Action<Transaction> action)
+        {
+            using (var tr = CADActive.Database.TransactionManager.StartTransaction())
+            {
+                // Invoke the method
+                action(tr);
+                tr.Commit();
+            }
+        }
+
+        public void ChangeSmallCircleToRed(Transaction tr)
+        {
+            // Get the block table for the current database
+            var blockTable = (BlockTable)tr.GetObject(CADActive.Database.BlockTableId, OpenMode.ForRead);
+            // Get the model space block table record
+            var modelSpace = (BlockTableRecord)tr.GetObject(blockTable[BlockTableRecord.ModelSpace], OpenMode.ForRead);
+            RXClass circleClass = RXObject.GetClass(typeof(Circle));
+            // Loop through the entities in model space
+            foreach (ObjectId objectId in modelSpace)
+            {
+                // Look for circles
+                if (objectId.ObjectClass.IsDerivedFrom(circleClass))
+                {
+                    var circle = (Circle)tr.GetObject(objectId, OpenMode.ForRead);
+                    if (circle.Radius < 1.0)
+                    {
+                        circle.UpgradeOpen();
+                        circle.ColorIndex = 1;
+                    }
+                }
+            }
+        }
+
+        [CommandMethod("ENTCOLOR3")]
+        public void ChangeEntityColorWithDelegate()
+        {
+            UsingTransaction(ChangeSmallCircleToRed);
+        }
+
+
+
     }
 }
