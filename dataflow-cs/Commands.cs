369f30448ea00eaf5c3073b52d159277c5eb5dcd
dalong <dalong0514@me.com>
2022-03-21 19:13:41 +0800

    从CommmonUtil项目引入CADActive静态类

--- dataflow-cs/Commands.cs
+++ dataflow-cs/Commands.cs
@@ -3,6 +3,7 @@ using Autodesk.AutoCAD.ApplicationServices;
 using Autodesk.AutoCAD.DatabaseServices;
 using Autodesk.AutoCAD.EditorInput;
 using Autodesk.AutoCAD.Runtime;
+using CommonUtils.CADUtils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -53,59 +54,15 @@ namespace dataflow_cs
             }
         }
 
-        /// runtime environment.
-        /// </summary>
-        public static class Active
-        {
-            /// <summary>
-            /// Returns the active Editor object.
-            /// </summary>
-            public static Editor Editor
-            {
-                get { return Document.Editor; }
-            }
-            /// <summary>
-            /// Returns the active Document object.
-            /// </summary>
-            public static Document Document
-            {
-                get { return Application.DocumentManager.MdiActiveDocument; }
-            }
-            /// <summary>
-            /// Returns the active Database object.
-            /// </summary>
-            public static Database Database
-            {
-                get { return Document.Database; }
-            }
-            /// <summary>
-            /// Sends a string to the command line in the active Editor
-            /// </summary>
-            /// <param name="message">The message to send.</param>
-            public static void WriteMessage(string message)
-            {
-                Editor.WriteMessage(message);
-            }
-            /// <summary>
-            /// Sends a string to the command line in the active Editor using String.Format.
-            /// </summary>
-            /// <param name="message">The message containing format specifications.</param>
-            /// <param name="parameter">The variables to substitute into the format string.</param>
-            public static void WriteMessage(string message, params object[] parameter)
-            {
-                Editor.WriteMessage(message, parameter);
-            }
-        }
-
         [CommandMethod("ENTCOLOR2")]
         public void ChangeEntityColor2()
         {
 
-            using (var tr = Active.Database.TransactionManager.StartTransaction())
+            using (var tr = CADActive.Database.TransactionManager.StartTransaction())
             {
 
                 // Get the block table for the current database
-                var blockTable = (BlockTable)tr.GetObject(Active.Database.BlockTableId, OpenMode.ForRead);
+                var blockTable = (BlockTable)tr.GetObject(CADActive.Database.BlockTableId, OpenMode.ForRead);
                 // Get the model space block table record
                 var modelSpace = (BlockTableRecord)tr.GetObject(blockTable[BlockTableRecord.ModelSpace], OpenMode.ForRead);
                 RXClass circleClass = RXObject.GetClass(typeof(Circle));
