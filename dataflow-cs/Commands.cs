91d7be1908cf9df07ff6f7bffdfc5a578db89fdc
dalong <dalong0514@me.com>
2022-03-22 09:40:42 +0800

    先删除委托代码

--- dataflow-cs/Commands.cs
+++ dataflow-cs/Commands.cs
@@ -27,7 +27,6 @@ namespace dataflow_cs
 
             using (var tr = CADActive.Database.TransactionManager.StartTransaction())
             {
-
                 // Get the block table for the current database
                 var blockTable = (BlockTable)tr.GetObject(CADActive.Database.BlockTableId, OpenMode.ForRead);
                 // Get the model space block table record
@@ -50,49 +49,5 @@ namespace dataflow_cs
                 tr.Commit();
             }
         }
-
-        public delegate void TransactionFunc(Action<Transaction> tr);
-
-        public void UsingTransaction(Action<Transaction> action)
-        {
-            using (var tr = CADActive.Database.TransactionManager.StartTransaction())
-            {
-                // Invoke the method
-                action(tr);
-                tr.Commit();
-            }
-        }
-
-        public void ChangeSmallCircleToRed(Transaction tr)
-        {
-            // Get the block table for the current database
-            var blockTable = (BlockTable)tr.GetObject(CADActive.Database.BlockTableId, OpenMode.ForRead);
-            // Get the model space block table record
-            var modelSpace = (BlockTableRecord)tr.GetObject(blockTable[BlockTableRecord.ModelSpace], OpenMode.ForRead);
-            RXClass circleClass = RXObject.GetClass(typeof(Circle));
-            // Loop through the entities in model space
-            foreach (ObjectId objectId in modelSpace)
-            {
-                // Look for circles
-                if (objectId.ObjectClass.IsDerivedFrom(circleClass))
-                {
-                    var circle = (Circle)tr.GetObject(objectId, OpenMode.ForRead);
-                    if (circle.Radius < 1.0)
-                    {
-                        circle.UpgradeOpen();
-                        circle.ColorIndex = 1;
-                    }
-                }
-            }
-        }
-
-        [CommandMethod("ENTCOLOR3")]
-        public void ChangeEntityColorWithDelegate()
-        {
-            UsingTransaction(ChangeSmallCircleToRed);
-        }
-
-
-
     }
 }
