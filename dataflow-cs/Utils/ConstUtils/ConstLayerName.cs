3688da15625949e35eb3796a440370db91ed4e69
dalong <dalong0514@126.com>
2025-04-13 23:56:22 +0800

    add constutils files

---
+++ dataflow-cs/Utils/ConstUtils/ConstLayerName.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace dataflow_cs.Utils.ConstUtils
+{
+    internal class ConstLayerName
+    {
+    }
+}
